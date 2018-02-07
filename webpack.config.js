946ee782cd8a9df91c4c72f4ec46e7c02e870648
Kelvin Li <jpkelvinli@gmail.com>
2018-02-07 01:40:20 -0800

    Add minification to  webpack build process.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-  entry: './build.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'p4.test.js'
-  }
+    entry: {
+        "p4": "./build.js",
+        "p4.min": "./build.js"
+    },
+    devtool: "inline-source-map",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js"
+    },
+    plugins: [
+        new UglifyJsPlugin({
+            include: /\.min\.js$/,
+            sourceMap: true
+        })
+    ]
 };
