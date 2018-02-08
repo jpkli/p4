75365c79f19144b64f5b1df8519b6bfd6a18dd88
Kelvin Li <jpkelvinli@gmail.com>
2018-02-07 16:30:58 -0800

    Add index.js for managing builds.

---
+++ index.js
@@ -0,0 +1,13 @@
+import p6 from './src/pipeline';
+import cstore    from './src/cstore';
+import * as ajax from './src/ajax';
+import parse     from './src/parse';
+
+var root = typeof self == 'object' && self.self === self && self ||
+           typeof global == 'object' && global.global === global && global ||
+           this;
+
+root.p6 = p6;
+root.p6.ajax = ajax;
+root.p6.cstore = cstore;
+root.p6.parse = parse;
