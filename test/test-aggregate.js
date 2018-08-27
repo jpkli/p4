850e22807e278e96cce15d189e2be71eefcb25a4
Kelvin Li <jpkelvinli@gmail.com>
2018-08-27 15:51:54 -0700

    Add unit test cases for cache and aggregation.

---
+++ test/test-aggregate.js
@@ -0,0 +1,60 @@
+import { assert } from 'chai';
+import { validate } from './utils';
+import cstore from '../src/cstore';
+import p4 from '../src/pipeline';
+import p3 from 'p.3';
+
+export default function({
+    data,
+    schema,
+    precision
+}) {
+    let db = cstore();
+    db.import({
+        data: data,
+        schema: schema
+    });
+
+    let config = {
+        container: "p4",
+        viewport: [800, 450]
+    };
+
+    let spec = [
+        {
+            $aggregate: {
+                $group: ['MotherRace', 'Gender'],
+                $reduce: {
+                    sumWeight: {$sum: 'Weight'},
+                    maxMotherWeight: {$max: 'MotherWeight'},
+                    count: {$count: '*'},
+                    // avergeAge: {$avg: 'MotherAge'},
+                }
+            }
+        }
+    ]
+
+    let gpu = p4(config).data(db.data());
+    let cpu = p3.pipeline(data);
+
+    let c = data.filter(d=>d.MotherRace == 'Black' && d.Gender == 'F').map(d=>d.Weight).reduce((a,b) => a+b);
+
+    describe('GPU-based aggregation', function() {
+        
+        describe('Group-by categorical attribute', function() {
+            let gpuResult = gpu.runSpec(spec).result('row').sort((a,b)=> a.count-b.count);
+            let cpuResult = cpu.runSpec(spec).sort((a,b)=> a.count-b.count);
+            console.log(gpuResult, cpuResult)
+            it('result size should equal ' + cpuResult.length, function() {
+                assert.equal(gpuResult.length, cpuResult.length);
+            });
+        
+            it('result should be closely equal with delta = ' + precision, function() {
+                validate(cpuResult, gpuResult, precision);
+            });
+        
+        });
+    
+    });
+
+}
---
+++ test/test-cache.js
@@ -0,0 +1,53 @@
+import { assert } from 'chai';
+import { validate } from './utils';
+import cstore from '../src/cstore';
+import p4 from '../src/pipeline';
+
+export default function({
+    data,
+    schema,
+    precision
+}) {
+    let db = cstore();
+    db.import({
+        data: data,
+        schema: schema
+    });
+
+    let cpuData = db.export();
+   
+    let config = {
+        container: "p4",
+        viewport: [800, 450]
+    };
+
+    let gpuData = p4(config)
+        .data(db.data())
+        .cache('initData')
+        .result('row')
+
+    describe('Cache data in system memory', function() {
+    
+        it('cached data size should equal ' + data.length, function() {
+            assert.equal(cpuData.length, data.length);
+        });
+    
+        it('result should be closely equal with delta = ' + precision, function() {
+            validate(data, cpuData, precision);
+        });
+    
+    });
+
+    describe('Cache data in GPU memory', function() {
+    
+        it('cached data size should equal ' + data.length, function() {
+            assert.equal(gpuData.length, data.length);
+        });
+    
+        it('result should be closely equal with delta = ' + precision, function() {
+            validate(data, gpuData, precision);
+        });
+    
+    });
+
+}
