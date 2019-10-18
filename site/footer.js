e078568c694bc9cb6021496a6fa0bddd861c61a3
Kelvin Li <jpkelvinli@gmail.com>
2019-10-18 01:22:16 -0700

    Add site footer.

---
+++ site/footer.js
@@ -0,0 +1,25 @@
+// import template from './html/footer.html';
+let template = `
+<div class="footer" style="width: 100%; text-align: center; bottom: 0;">
+<hr />
+<p>
+    &copy; 2017-2019 P4 is developed by <a href="https://github.com/jpkli">Kelvin Li</a>
+</p>
+</div>
+`
+
+export default {
+  name: 'Footer',
+  template,
+  props: {
+
+  },
+  data() {
+    return {
+    }
+  },
+
+  created() {
+
+  }
+}
