27ef772b8403286a18fe8e00c2ba865c6d7cc34b
Kelvin Li <jpkelvinli@gmail.com>
2020-07-28 22:36:45 -0700

    Include a list of supported operations for quick lookup.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cstore from './src/cstore';
 import  * as ctypes  from './src/ctypes';
 import * as ajax from './src/io/ajax';
 import parse from './src/io/parse';
+import kernels from './src/kernels';
 
 var root = typeof self == 'object' && self.self === self && self ||
            typeof global == 'object' && global.global === global && global ||
@@ -12,6 +13,7 @@ p4.ajax = ajax;
 p4.cstore = cstore;
 p4.ctypes = ctypes;
 p4.parse = parse;
+p4.operations = Object.keys(kernels);
 
 root.p4 = p4;
 export default root.p4;
